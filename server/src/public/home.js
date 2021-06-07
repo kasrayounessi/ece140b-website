9083b0e7c36dfdbc7d7d941eda77a6b2b9e4ba7a
kasrayounessi <76799081+kasrayounessi@users.noreply.github.com>
2021-06-07 02:09:45 -0700

    Add files via upload

---
+++ server/src/public/home.js
@@ -0,0 +1,298 @@
+function welcomeToggle(){
+    document.getElementById("one").style.opacity = 1;
+    document.getElementById("two").style.opacity = 0;
+    document.getElementById("three").style.opacity = 0;
+    document.getElementById("four").style.opacity = 0;
+    document.getElementById("five").style.opacity = 0;
+    document.getElementById("six").style.opacity = 0;
+    document.getElementById("seven").style.opacity = 0;
+    
+}
+  
+function cvToggle(){
+    document.getElementById("one").style.opacity = 0;
+    document.getElementById("two").style.opacity = 1;
+    document.getElementById("three").style.opacity = 0;
+    document.getElementById("four").style.opacity = 0;
+    document.getElementById("five").style.opacity = 0;
+    document.getElementById("six").style.opacity = 0;
+    document.getElementById("seven").style.opacity = 0;
+    
+}
+  
+function avatarToggle(){
+    document.getElementById("one").style.opacity = 0;
+    document.getElementById("two").style.opacity = 0;
+    document.getElementById("three").style.opacity = 1;
+    document.getElementById("four").style.opacity = 0;
+    document.getElementById("five").style.opacity = 0;
+    document.getElementById("six").style.opacity = 0;
+    document.getElementById("seven").style.opacity = 0;
+    
+}
+  
+function personalToggle(){
+    document.getElementById("one").style.opacity = 0;
+    document.getElementById("two").style.opacity = 0;
+    document.getElementById("three").style.opacity = 0;
+    document.getElementById("four").style.opacity = 1;
+    document.getElementById("five").style.opacity = 0;
+    document.getElementById("six").style.opacity = 0;
+    document.getElementById("seven").style.opacity = 0;
+    
+}
+  
+function educationToggle(){
+    document.getElementById("one").style.opacity = 0;
+    document.getElementById("two").style.opacity = 0;
+    document.getElementById("three").style.opacity = 0;
+    document.getElementById("four").style.opacity = 0;
+    document.getElementById("five").style.opacity = 1;
+    document.getElementById("six").style.opacity = 0;
+    document.getElementById("seven").style.opacity = 0;
+    
+}
+  
+function projectToggle(){
+    document.getElementById("one").style.opacity = 0;
+    document.getElementById("two").style.opacity = 0;
+    document.getElementById("three").style.opacity = 0;
+    document.getElementById("four").style.opacity = 0;
+    document.getElementById("five").style.opacity = 0;
+    document.getElementById("six").style.opacity = 1;
+    document.getElementById("seven").style.opacity = 0;
+   
+    
+}
+  
+function aboutToggle(){
+    document.getElementById("one").style.opacity = 0;
+    document.getElementById("two").style.opacity = 0;
+    document.getElementById("three").style.opacity = 0;
+    document.getElementById("four").style.opacity = 0;
+    document.getElementById("five").style.opacity = 0;
+    document.getElementById("six").style.opacity = 0;
+    document.getElementById("seven").style.opacity = 1;
+  
+    
+}
+  
+  
+function mouseOut(){
+    document.getElementById("one").style.opacity = 0;
+    document.getElementById("two").style.opacity = 0;
+    document.getElementById("three").style.opacity = 0;
+    document.getElementById("four").style.opacity = 0;
+    document.getElementById("five").style.opacity = 0;
+    document.getElementById("six").style.opacity = 0;
+    document.getElementById("seven").style.opacity = 0;
+    
+}
+
+function flintTropicsClick(){
+    document.getElementById("one-c").style.opacity = 1;
+    document.getElementById("two-c").style.opacity = 0;
+    document.getElementById("three-c").style.opacity = 0;
+    document.getElementById("four-c").style.opacity = 0;
+    document.getElementById("five-c").style.opacity = 0;
+    document.getElementById("six-c").style.opacity = 0;
+    document.getElementById("seven-c").style.opacity = 0;
+    document.getElementById("eight-c").style.opacity = 0;
+    document.getElementById("nine-c").style.opacity = 0;
+    document.getElementById("ten-c").style.opacity = 0;
+    document.getElementById("eleven-c").style.opacity = 0;
+    document.getElementById("twelve-c").style.opacity = 0;
+    document.getElementById("thirteen-c").style.opacity = 0;
+}
+  
+function triTechClicks(){
+    document.getElementById("one-c").style.opacity = 0;
+    document.getElementById("two-c").style.opacity = 1;
+    document.getElementById("three-c").style.opacity = 0;
+    document.getElementById("four-c").style.opacity = 0;
+    document.getElementById("five-c").style.opacity = 0;
+    document.getElementById("six-c").style.opacity = 0;
+    document.getElementById("seven-c").style.opacity = 0;
+    document.getElementById("eight-c").style.opacity = 0;
+    document.getElementById("nine-c").style.opacity = 0;
+    document.getElementById("ten-c").style.opacity = 0;
+    document.getElementById("eleven-c").style.opacity = 0;
+    document.getElementById("twelve-c").style.opacity = 0;
+    document.getElementById("thirteen-c").style.opacity = 0;
+}
+  
+function ruffRidesClick(){
+    document.getElementById("one-c").style.opacity = 0;
+    document.getElementById("two-c").style.opacity = 0;
+    document.getElementById("three-c").style.opacity = 1;
+    document.getElementById("four-c").style.opacity = 0;
+    document.getElementById("five-c").style.opacity = 0;
+    document.getElementById("six-c").style.opacity = 0;
+    document.getElementById("seven-c").style.opacity = 0;
+    document.getElementById("eight-c").style.opacity = 0;
+    document.getElementById("nine-c").style.opacity = 0;
+    document.getElementById("ten-c").style.opacity = 0;
+    document.getElementById("eleven-c").style.opacity = 0;
+    document.getElementById("twelve-c").style.opacity = 0;
+    document.getElementById("thirteen-c").style.opacity = 0;
+}
+  
+function kyteClick(){
+    document.getElementById("one-c").style.opacity = 0;
+    document.getElementById("two-c").style.opacity = 0;
+    document.getElementById("three-c").style.opacity = 0;
+    document.getElementById("four-c").style.opacity = 1;
+    document.getElementById("five-c").style.opacity = 0;
+    document.getElementById("six-c").style.opacity = 0;
+    document.getElementById("seven-c").style.opacity = 0;
+    document.getElementById("eight-c").style.opacity = 0;
+    document.getElementById("nine-c").style.opacity = 0;
+    document.getElementById("ten-c").style.opacity = 0;
+    document.getElementById("eleven-c").style.opacity = 0;
+    document.getElementById("twelve-c").style.opacity = 0;
+    document.getElementById("thirteen-c").style.opacity = 0;
+}
+  
+function lookAtYourselfClick(){
+    document.getElementById("one-c").style.opacity = 0;
+    document.getElementById("two-c").style.opacity = 0;
+    document.getElementById("three-c").style.opacity = 0;
+    document.getElementById("four-c").style.opacity = 0;
+    document.getElementById("five-c").style.opacity = 1;
+    document.getElementById("six-c").style.opacity = 0;
+    document.getElementById("seven-c").style.opacity = 0;
+    document.getElementById("eight-c").style.opacity = 0;
+    document.getElementById("nine-c").style.opacity = 0;
+    document.getElementById("ten-c").style.opacity = 0;
+    document.getElementById("eleven-c").style.opacity = 0;
+    document.getElementById("twelve-c").style.opacity = 0;
+    document.getElementById("thirteen-c").style.opacity = 0;
+}
+  
+function sentinelClick(){
+    document.getElementById("one-c").style.opacity = 0;
+    document.getElementById("two-c").style.opacity = 0;
+    document.getElementById("three-c").style.opacity = 0;
+    document.getElementById("four-c").style.opacity = 0;
+    document.getElementById("five-c").style.opacity = 0;
+    document.getElementById("six-c").style.opacity = 1;
+    document.getElementById("seven-c").style.opacity = 0;
+    document.getElementById("eight-c").style.opacity = 0;
+    document.getElementById("nine-c").style.opacity = 0;
+    document.getElementById("ten-c").style.opacity = 0;
+    document.getElementById("eleven-c").style.opacity = 0;
+    document.getElementById("twelve-c").style.opacity = 0;
+    document.getElementById("thirteen-c").style.opacity = 0;
+}
+  
+function recipeMasterClick(){
+    document.getElementById("one-c").style.opacity = 0;
+    document.getElementById("two-c").style.opacity = 0;
+    document.getElementById("three-c").style.opacity = 0;
+    document.getElementById("four-c").style.opacity = 0;
+    document.getElementById("five-c").style.opacity = 0;
+    document.getElementById("six-c").style.opacity = 0;
+    document.getElementById("seven-c").style.opacity = 1;
+    document.getElementById("eight-c").style.opacity = 0;
+    document.getElementById("nine-c").style.opacity = 0;
+    document.getElementById("ten-c").style.opacity = 0;
+    document.getElementById("eleven-c").style.opacity = 0;
+    document.getElementById("twelve-c").style.opacity = 0;
+    document.getElementById("thirteen-c").style.opacity = 0;
+}
+  
+function smartRepzClick(){
+    document.getElementById("one-c").style.opacity = 0;
+    document.getElementById("two-c").style.opacity = 0;
+    document.getElementById("three-c").style.opacity = 0;
+    document.getElementById("four-c").style.opacity = 0;
+    document.getElementById("five-c").style.opacity = 0;
+    document.getElementById("six-c").style.opacity = 0;
+    document.getElementById("seven-c").style.opacity = 0;
+    document.getElementById("eight-c").style.opacity = 1;
+    document.getElementById("nine-c").style.opacity = 0;
+    document.getElementById("ten-c").style.opacity = 0;
+    document.getElementById("eleven-c").style.opacity = 0;
+    document.getElementById("twelve-c").style.opacity = 0;
+    document.getElementById("thirteen-c").style.opacity = 0;
+}
+  
+function everybodyLovesRamsinClick(){
+    document.getElementById("one-c").style.opacity = 0;
+    document.getElementById("two-c").style.opacity = 0;
+    document.getElementById("three-c").style.opacity = 0;
+    document.getElementById("four-c").style.opacity = 0;
+    document.getElementById("five-c").style.opacity = 0;
+    document.getElementById("six-c").style.opacity = 0;
+    document.getElementById("seven-c").style.opacity = 0;
+    document.getElementById("eight-c").style.opacity = 0;
+    document.getElementById("nine-c").style.opacity = 1;
+    document.getElementById("ten-c").style.opacity = 0;
+    document.getElementById("eleven-c").style.opacity = 0;
+    document.getElementById("twelve-c").style.opacity = 0;
+    document.getElementById("thirteen-c").style.opacity = 0;
+}
+  
+function ezTraderClick(){
+    document.getElementById("one-c").style.opacity = 0;
+    document.getElementById("two-c").style.opacity = 0;
+    document.getElementById("three-c").style.opacity = 0;
+    document.getElementById("four-c").style.opacity = 0;
+    document.getElementById("five-c").style.opacity = 0;
+    document.getElementById("six-c").style.opacity = 0;
+    document.getElementById("seven-c").style.opacity = 0;
+    document.getElementById("eight-c").style.opacity = 0;
+    document.getElementById("nine-c").style.opacity = 0;
+    document.getElementById("ten-c").style.opacity = 1;
+    document.getElementById("eleven-c").style.opacity = 0;
+    document.getElementById("twelve-c").style.opacity = 0;
+    document.getElementById("thirteen-c").style.opacity = 0;
+}
+  
+function repairBoizClick(){
+    document.getElementById("one-c").style.opacity = 0;
+    document.getElementById("two-c").style.opacity = 0;
+    document.getElementById("three-c").style.opacity = 0;
+    document.getElementById("four-c").style.opacity = 0;
+    document.getElementById("five-c").style.opacity = 0;
+    document.getElementById("six-c").style.opacity = 0;
+    document.getElementById("seven-c").style.opacity = 0;
+    document.getElementById("eight-c").style.opacity = 0;
+    document.getElementById("nine-c").style.opacity = 0;
+    document.getElementById("ten-c").style.opacity = 0;
+    document.getElementById("eleven-c").style.opacity = 1;
+    document.getElementById("twelve-c").style.opacity = 0;
+    document.getElementById("thirteen-c").style.opacity = 0;
+}
+  
+function CCJClick(){
+    document.getElementById("one-c").style.opacity = 0;
+    document.getElementById("two-c").style.opacity = 0;
+    document.getElementById("three-c").style.opacity = 0;
+    document.getElementById("four-c").style.opacity = 0;
+    document.getElementById("five-c").style.opacity = 0;
+    document.getElementById("six-c").style.opacity = 0;
+    document.getElementById("seven-c").style.opacity = 0;
+    document.getElementById("eight-c").style.opacity = 0;
+    document.getElementById("nine-c").style.opacity = 0;
+    document.getElementById("ten-c").style.opacity = 0;
+    document.getElementById("eleven-c").style.opacity = 0;
+    document.getElementById("twelve-c").style.opacity = 1;
+    document.getElementById("thirteen-c").style.opacity = 0;
+}
+  
+function industrialIllusionsClick(){
+    document.getElementById("one-c").style.opacity = 0;
+    document.getElementById("two-c").style.opacity = 0;
+    document.getElementById("three-c").style.opacity = 0;
+    document.getElementById("four-c").style.opacity = 0;
+    document.getElementById("five-c").style.opacity = 0;
+    document.getElementById("six-c").style.opacity = 0;
+    document.getElementById("seven-c").style.opacity = 0;
+    document.getElementById("eight-c").style.opacity = 0;
+    document.getElementById("nine-c").style.opacity = 0;
+    document.getElementById("ten-c").style.opacity = 0;
+    document.getElementById("eleven-c").style.opacity = 0;
+    document.getElementById("twelve-c").style.opacity = 0;
+    document.getElementById("thirteen-c").style.opacity = 1;
+}
